b84060a97fc480cee84dec633afad51a0b924d50
echter <chrech.birr@gmail.com>
2021-03-15 22:27:33 +0100

    basic procedural generation feature created

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int xSize = 20;
+    public int zSize = 20;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        // StartCoroutine(CreateShape());
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void Update()
+    {
+        // UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        int i = 0;
+        for (int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                y += Mathf.PerlinNoise(x * .05f, z * .05f) * 10f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+        foreach (Vector3 vertex in vertices)
+        {
+            Debug.Log(vertex);
+        }
+    }
+
+    IEnumerator SlowCreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        int i = 0;
+        for (int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+                yield return new WaitForSeconds(0.0005f);
+            }
+            vert++;
+        }
+
+        foreach (Vector3 vertex in vertices)
+        {
+            Debug.Log(vertex);
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        mesh.RecalculateBounds();
+        MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+    }
+}
